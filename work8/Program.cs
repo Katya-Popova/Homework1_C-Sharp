0f30aaf17cd9cb1461f5a0d227f1b1d56007edc1
Katya Popova <Katya_ Popova@mail.ru>
2022-09-19 20:02:15 +0300

    четное в диапозоне

---
+++ work8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int number0 = 2;
+while (number0 < number)
+{
+    Console.WriteLine(number0);
+    number0=number0+2;
+}
