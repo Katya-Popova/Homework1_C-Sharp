88c95af83b7c1d2c7bab392a1405b304ac22c91b
Katya Popova <Katya_ Popova@mail.ru>
2022-09-19 16:37:11 +0300

    почему не применяется numbermax

---
+++ work4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Эта программа покажет наибольшее из 3-х чисел");
+Console.Write("Введите первое число ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число ");
+int numberC = int.Parse(Console.ReadLine());
+
+int numbermax = numberA;
+
+if (numberA < numberB)
+{
+     int numbermax = numberB;
+}
+
+else if (numbermax < numberC)
+{
+    int numbermax = numberC;
+}
+
+Console.WriteLine("Максимальное число из этих чисел " + numbermax);
