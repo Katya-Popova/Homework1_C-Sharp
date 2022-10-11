7d119a40e8fd7a482b6c952b9feb9361d1b6150d
Katya Popova <Katya_ Popova@mail.ru>
2022-10-11 15:23:45 +0300

    zadacha

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -48,4 +48,18 @@ Zadacha10();
 Zadacha13();       
 
 Zadacha15();
-  
+      // ЗНапишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вы ввели число: " + number + ". Являетслся ли оно палиндромом?");
+int number1 = number /10000;
+int number2 = number / 1000 % 10;
+int number4 = number / 10 % 10;
+int number5 = number % 10;
+if (number1 == number5 && number2 == number4)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}    
